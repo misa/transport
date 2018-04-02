a84d997435653aec6880b7774514a8412d2c46bd
Michal Kocian <michal@kocian.name>
2018-04-02 12:15:35 +0100

    Make a constuctor field private

--- presentation/src/androidTest/java/name/kocian/tfl/presentation/util/JsonFile.kt
+++ presentation/src/androidTest/java/name/kocian/tfl/presentation/util/JsonFile.kt
@@ -2,7 +2,7 @@ package name.kocian.tfl.presentation.util
 
 import android.support.test.InstrumentationRegistry
 
-class JsonFile(val name: String) {
+class JsonFile(private val name: String) {
 
     fun load() = InstrumentationRegistry.getContext().assets.open(name).use {
         it.reader().readText()
